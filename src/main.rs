60e6a4abd29c07848fe040d7e3ad34812cc4eab0
Sachin <67502584+Linux-Server@users.noreply.github.com>
2022-08-23 10:58:21 +0000

    impl function implementation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::process;
 
 fn main(){
-  let arguments = read_arguments();
+  let arguments = InputArguments::read_arguments();
   println!("Started to red file data from env argumets: {:?}", arguments);
 
 }
@@ -14,6 +14,25 @@ struct InputArguments {
   file_path : String
 }
 
+
+// alternate option for a regular funtion
+impl InputArguments{
+  fn read_arguments()-> Result<InputArguments,String>{
+     let arguments: Vec<String> = env::args().collect();
+     if arguments.len()<3{
+       return Err("Total Input Arguments Errror".to_string())
+     }
+     let query = arguments[1].clone();
+     let file_path = arguments[2].clone();
+     Ok(InputArguments{
+       query,
+       file_path
+     })
+  }
+}
+
+
+// read arguments in normal functional way
 fn read_arguments()-> InputArguments{
   let arguments: Vec<String> = env::args().collect();
   if arguments.len() < 3{
